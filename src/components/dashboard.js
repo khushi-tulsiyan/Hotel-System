4a51485ae825a2f149e462ba5cbed73ba35c3640
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2024-07-11 16:57:48 +0530

    new files

---
+++ src/components/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const dashboard = () => {
+    return (
+        <div>
+            <h1>Hotel Management Dashboard</h1>
+            <div className="activity-summary">
+                {}
+            </div>
+        </div>
+    );
+};
+
+export default dashboard;
